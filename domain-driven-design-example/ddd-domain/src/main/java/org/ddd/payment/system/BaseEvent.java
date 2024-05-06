86ad185de805d7bdbd34f9fabdeeb37fa606b906
bert82503 <bert825_work@163.com>
2024-05-06 13:59:53 +0800

    Add BaseStatus, BaseEvent

---
+++ domain-driven-design-example/ddd-domain/src/main/java/org/ddd/payment/system/BaseEvent.java
@@ -0,0 +1,10 @@
+package org.ddd.payment.system;
+
+/**
+ * 事件基类
+ *
+ * @since 2024/5/6
+ */
+public interface BaseEvent {
+    // marker
+}
---
+++ domain-driven-design-example/ddd-domain/src/main/java/org/ddd/payment/system/BaseStatus.java
@@ -0,0 +1,10 @@
+package org.ddd.payment.system;
+
+/**
+ * 状态基类
+ *
+ * @since 2024/5/6
+ */
+public interface BaseStatus {
+    // marker
+}
